b54de12f74dd0b84717fc060496a269b002b27f9
irodaberdieva069-glitch <irodaberdieva069@gmail.com>
2026-05-25 13:27:57 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+# Madina uchun maxsus matn
+madina_text = """
+Har bir insonning hayotida shunday nomlar borki, ular shunchaki so‘z emas, 
+balki qalbga iliqlik bag‘ishlaydigan xotiralardir. 
+
+Madina – bu ismning o‘zida qandaydir yorug‘lik va poklik bor. 
+U bilan suhbatlashganda dunyo tashvishlari bir zumga chekinib, 
+barcha narsa o‘z o‘rniga tushgandek tuyuladi. 
+
+Inson ba’zida o‘zining ichki dunyosini, orzu-maqsadlarini tushunadigan, 
+har doim qo‘llab-quvvatlaydigan va ishonch bilan qaraydigan do‘stga ehtiyoj sezadi. 
+Madina esa ana shunday qalbi go‘zal, har bir so‘zida haqiqat va samimiyatni 
+jo etgan insonlardan. U bilan birga har bir daqiqa, har bir kichik suhbat ham qadrli. 
+Hayotda bunday insonlar bilan yo‘l kesishishi – chinakam omad.
+"""
+
+# Konsolga chiqarish
+def display_message():
+    print("-" * 30)
+    print(madina_text.strip())
+    print("-" * 30)
+
+if __name__ == "__main__":
+    display_message()
